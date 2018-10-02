3fe694e0811e29784929b8f5af094ffe391ee6c5
Gaurav <gdwadhwani@gmail.com>
2018-10-02 00:17:16 -0400

    test
    test

---
+++ TestAppDev/app/src/main/java/com/example/gdwadhwani/testappdev/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gdwadhwani.testappdev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.support_simple_spinner_dropdown_item);
+    }
+}
